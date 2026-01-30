f7d3bf8c733cfdff9afdcab190d0aa670c2fa3ce
eslamabotaleb <eslamabotaleb07@gmail.com>
2026-01-30 02:46:49 +0200

    prepared NetworkProvider

---
+++ CountriesApp/Infrastructure/Network/NetworkProvider.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkProvider.swift
+//  CountriesApp
+//
+//  Created by Eslam Abotaleb on 30/01/2026.
+//
+
+import Foundation
+
+final class NetworkProvider {
+    static let shared = NetworkProvider()
+
+    private let client: NetworkClient
+    private let _context: CountriesContext
+
+    var context: CountriesContext { _context }
+
+    private init() {
+        _context = CountriesContext(
+            networkContext: NetworkContext(
+                baseURLString: EnvironmentApp.apiUrl)
+        )
+
+        client = NetworkClient(
+            context: _context,
+            requestModifiers: [
+                AuthorizationModifier(),
+                LanguageModifier()
+            ]
+        )
+    }
+
+    func makeClient() -> NetworkClient {
+        return client
+    }
+}
