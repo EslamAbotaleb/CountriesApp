86f19821a58d16302e4dcf99b3e1e619e81c61f3
eslamabotaleb <eslamabotaleb07@gmail.com>
2026-01-30 02:08:30 +0200

    Initial Commit

---
+++ CountriesApp/CountriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesAppApp.swift
+//  CountriesApp
+//
+//  Created by Eslam Abotaleb on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct CountriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountriesAppTests/CountriesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesAppTests.swift
+//  CountriesAppTests
+//
+//  Created by Eslam Abotaleb on 30/01/2026.
+//
+
+import Testing
+@testable import CountriesApp
+
+struct CountriesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
